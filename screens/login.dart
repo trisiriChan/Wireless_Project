c12e26cf15ac6b552d507b82d13807b6dd120a14
trisiriChan <88642560+trisiriChan@users.noreply.github.com>
2022-02-17 23:14:56 +0700

    Add files via upload
    6288100 & 6288158

---
+++ screens/login.dart
@@ -0,0 +1,51 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatelessWidget {
+  const MyLogin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(80.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome to world bank',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              Image.asset(
+                'img/pill.jpg',
+                
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Member name',
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                child: const Text('ENTER'),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/menu');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.yellow,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
